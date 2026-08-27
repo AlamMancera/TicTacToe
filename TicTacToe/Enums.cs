33720c1f21914e6e97e4e0b16d71560e8307e3ee
Alam <mczynko@hotmail.com>
2026-08-27 01:10:08 -0600

    feat: agregar EstadoJuego enum

---
+++ TicTacToe/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    enum EstadoJuego
+    {
+        EnCurso,
+        GanadorJugador1,
+        GanadorJugador2,
+        Empate
+    }
+}
